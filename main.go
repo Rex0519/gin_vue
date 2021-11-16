757db45e89d3bfef1fb14310218caf7d17498dcd
rex0519 <56961205+Rex0519@users.noreply.github.com>
2021-11-16 15:12:09 +0800

    123

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("hello motherfucker!!!")
 	fmt.Println("hello motherfucker!!!")
-
+	fmt.Println("hello motherfucker!!!")
 	fmt.Println("hello motherfucker!!!")
 }
